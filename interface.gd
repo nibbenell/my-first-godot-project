f21b36bb1e9c8fa12f99d3c88d1d5c7d382e4175
Ilnur Khisai <ilnur.khisaev@gmail.com>
2023-05-28 22:13:52 +0500

    var

--- interface.gd
+++ interface.gd
@@ -5,11 +5,12 @@ var click_count = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$ClickCountLabel.text = str(click_count)
+	#$ClickCountLabel.text = str(click_count)
+	print("Hello world!")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	$VBoxContainer/ClickCountLabel.text = str(click_count)
 
 
 func _on_click_button_pressed():
